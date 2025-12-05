1398d2dd424da9c24bdecff8bd3477527b5546c7
HaritzGarai <hgaraim@gmail.com>
2025-12-05 15:58:04 +0000

    Deploying to gh-pages from @ HaritzGarai/Test@5f04cdf682999476ae7ded067220b972eb944e78 🚀

---
+++ src/messages.f90
@@ -0,0 +1,22 @@
+module messages
+
+      implicit none
+
+      public
+
+      character(len=254), parameter :: hello="Hello!"
+      !! Hi in English
+      character(len=254), parameter :: kaixo="Kaixo!"
+      !! Hi in Basque
+      character(len=254), parameter :: hallo="Hallo!"
+      !! Hi in German
+      character(len=254), parameter :: hola="Hola!"
+      !! Hi in Spanish
+      character(len=254), parameter :: goodby="Goodby!"
+      !! By in English
+      character(len=254), parameter :: agur="Agur!"
+      !! By in Basque
+      character(len=254), parameter :: adios="Adios!"
+      !! By in Spanish
+
+end module messages
